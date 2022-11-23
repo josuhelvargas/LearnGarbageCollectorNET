8d77fa49a98e955ee701cbde4881615568cd1ff4
dav1nc1vargas <josuhelvargas@gmail.com>
2022-11-23 14:55:08 -0600

    First commit see together with Notion to learn how Garbage Collector .NET

---
+++ LearnGarbageCollector/ChapterNotes/StackOverflow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGarbageCollector.ChapterNotes
+{
+    public class StackOverflow //This Stack overflow Exception happens when a method is called infinite number of times.Every time we go out of a method their variables  are destroyed
+    {
+        public void InfinteReference()
+        {
+            Console.WriteLine("This is a eternal circular shitty reference so stack overflow exception its throwed!");
+            //InfinteReference();
+        }
+    }
+}
